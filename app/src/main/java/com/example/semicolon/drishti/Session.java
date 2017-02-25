c2970408dcf75e80f267b63c898ae00ea4ccdd18
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2017-02-25 09:16:20 +0530

    Add Camera Preview to Activity

--- app/src/main/java/com/example/semicolon/drishti/Session.java
+++ app/src/main/java/com/example/semicolon/drishti/Session.java
@@ -1,10 +1,195 @@
 package com.example.semicolon.drishti;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by semicolon on 2/25/2017.
  */
 
 public class Session extends AppCompatActivity {
+
+    public static final String TAG = "SessionActivity";
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    FrameLayout preview;
+    private GestureDetectorCompat mDetector;
+    ImageView camera_image_preview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session);
+
+
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+
+
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == 1) {
+            //Handle Portrait views here
+
+        } else if (orientation == 2) {
+            //Handle Landscape views here
+
+            mCamera = getCameraInstance();
+            mPreview = new CameraPreview(this, mCamera);
+            preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+            camera_image_preview = (ImageView) findViewById(R.id.camera_image_preview);
+            //Set camera to continually auto-focus
+            Camera.Parameters params = mCamera.getParameters();
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            mCamera.setParameters(params);
+
+            preview.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    mDetector.onTouchEvent(motionEvent);
+                    return true;
+                }
+            });
+
+
+        } else {
+            //Fallback to Portrait?
+        }
+
+
+    }
+
+    //Get a camera instance here
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDoubleTap(MotionEvent event) {
+            Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+            mCamera.takePicture(shutterCallback, rawCallback, jpegCallback);
+
+            return true;
+        }
+
+    }
+
+    Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
+        public void onShutter() {
+        }
+    };
+
+    Camera.PictureCallback rawCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+        }
+    };
+
+    Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            new SaveImageTask().execute(data);
+            resetCam();
+            Log.d(TAG, "onPictureTaken - jpeg");
+        }
+    };
+
+    private void resetCam() {
+        mCamera.startPreview();
+    }
+
+
+    private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+
+        String fileName;
+        File outFile;
+        FileOutputStream outStream = null;
+        Bitmap myBitmap;
+
+
+        public SaveImageTask() {
+
+        }
+
+
+        @Override
+        protected Void doInBackground(byte[]... data) {
+
+            // Write to SD Card
+            try {
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File(sdCard.getAbsolutePath() + "/semicolons");
+                dir.mkdirs();
+
+                fileName = String.format("%d.jpg", System.currentTimeMillis());
+                outFile = new File(dir, fileName);
+
+                outStream = new FileOutputStream(outFile);
+                outStream.write(data[0]);
+                outStream.flush();
+                outStream.close();
+
+                Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            myBitmap = BitmapFactory.decodeFile(outFile.getAbsolutePath());
+            camera_image_preview.setVisibility(View.VISIBLE);
+            camera_image_preview.setImageBitmap(myBitmap);
+            // Execute some code after 5 seconds have passed
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    camera_image_preview.setVisibility(View.INVISIBLE);
+
+                }
+            }, 1000);
+
+            //TODO: Change to File if needed
+//            ImageUploadAsyncTask imageUploadAsyncTask = new ImageUploadAsyncTask();
+//            imageUploadAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, outFile.getAbsolutePath());
+
+        }
+
+
+    }
+
 }
