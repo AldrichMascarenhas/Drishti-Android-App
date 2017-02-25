9b2e668211a2cfbc72fa7a7877d5cc179ed9e893
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2017-02-25 16:10:15 +0530

    Add buss Files

---
+++ app/src/main/java/com/example/semicolon/drishti/CONFIG.java
@@ -0,0 +1,12 @@
+package com.example.semicolon.drishti;
+
+/**
+ * Created by semicolon on 2/25/2017.
+ */
+
+public class CONFIG {
+
+    public static  final String ACTUAL_HOST = "http://35.185.68.150:32768/";
+    public static  final String DUMMY_HOST = "http://35.185.68.150:32768/";
+
+}
