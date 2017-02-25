ca4750665f330c4b2538e1c9f6c65018f94b4214
semicolon <semicolon>
2017-02-25 08:33:05 +0530

    Added Permissions and Initial Activities

---
+++ app/src/main/java/com/example/semicolon/drishti/Session.java
@@ -0,0 +1,10 @@
+package com.example.semicolon.drishti;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by semicolon on 2/25/2017.
+ */
+
+public class Session extends AppCompatActivity {
+}
